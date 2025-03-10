a13039deefdd743c052cdadbcc4c16f1ecdeba4e
abhis12321 <abhisheksingh2359328@gmail.com>
2025-03-10 12:00:57 +0530

    solution for Partition Equal Subset Sum (Medium)

--- medium/Partition Equal Subset Sum.java
+++ medium/Partition Equal Subset Sum.java
@@ -1,36 +1,29 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = sum(nums);
-        if((sum & 1) == 1) {
+        int sum = 0;
+        int max = 0;
+
+        for(int m : nums) {
+            sum += m;
+            max = Math.max(max , m);
+        }
+
+        if(sum % 2 == 1 || max > sum / 2) {
             return false;
         }
 
-        int half = sum / 2;
-        boolean dp[] = new boolean[half + 1];
-        
+        sum /= 2;
+        boolean dp[] = new boolean[sum+1];
         dp[0] = true;
 
         for(int m : nums) {
-            if(m <= half) {
-                for(int j = half; j >= m; j--) {
-                    if(dp[j-m]) {
-                        dp[j] = true;
-                    }
+            for(int j = sum; j >= m; j--) {
+                if(dp[j-m]) {
+                    dp[j] = true;
                 }
-                dp[m] = true;
-            } else {
-                return false;
             }
         }
-        // System.out.println(Arrays.toString(dp));
-        return dp[half];
-    }
 
-    private int sum(int nums[])  {
-        int sum = 0;
-        for(int m : nums) {
-            sum += m;
-        }
-        return sum;
+        return dp[sum];
     }
 }
