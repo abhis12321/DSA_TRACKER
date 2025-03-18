d49deb477ec66812243548c55143ccd884044b69
abhis12321 <abhisheksingh2359328@gmail.com>
2025-03-18 10:05:29 +0530

    solution for Longest Increasing Subsequence (Medium)

--- medium/Longest Increasing Subsequence.java
+++ medium/Longest Increasing Subsequence.java
@@ -3,9 +3,9 @@ class Solution {
         int dp[] = new int[nums.length];
         Arrays.fill(dp , 1);
         int max = 1;
-        for(int i = nums.length-1; i > 0; i--) {
-            for(int j = i-1; j >= 0; j--) {
-                if(nums[j] < nums[i]) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[j] > nums[i]) {
                     dp[j] = Math.max(dp[j] , 1+dp[i]);
                     max = Math.max(max , dp[j]);
                 }
